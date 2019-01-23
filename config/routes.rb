65b9d46ee143d0a410e135c608aaf497ea970da2
pengxian <qpxblue@aliyun.com>
2019-01-23 21:31:14 +0800

    users_controller_spec

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
 
   namespace :api, defaults: { format: :json } do
     scope module: :v1 do
-      resources :users, only: [:show]
+      resources :users, only: [:show, :create]
     end
   end
 
--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -7,11 +7,36 @@ RSpec.describe Api::V1::UsersController, type: :controller do
       @user = create :user
       get :show, params: { id: @user.id }
     end
-
     it { should respond_with 200 }
     it 'returns a user response' do
       json_response = JSON.parse response.body, symbolize_names: true
       expect(json_response[:email]).to eq @user.email
+    end
+  end
+
+  describe 'Post #create' do
+    context 'when create success' do
+      before :each do
+        @user_attributes = attributes_for :user
+        post :create, params: { user: @user_attributes }
+      end
+      it { should respond_with 201 }
+      it 'returns the user record' do
+        json_response = JSON.parse response.body, symbolize_names: true
+        expect(json_response[:email]).to eq @user_attributes[:email]
+      end
+    end
+
+    context 'when create fail' do
+      before :each do
+        @invaild_user_attributes = { password: '123456', password_confirmation: '123456' }
+        post :create, params: { user: @invaild_user_attributes }
+      end
+      it { should respond_with 422 }
+      it 'reader error json' do
+        json_response = JSON.parse response.body, symbolize_names: true
+        expect(json_response[:errors][:email]).to include("can't be blank")
+      end
     end
 
   end
