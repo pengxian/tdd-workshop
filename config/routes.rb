6d5e371d7927661ff6e66f953b86854f1848520b
pengxian <qpxblue@aliyun.com>
2019-01-23 20:58:47 +0800

    users_controller_spec

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,11 @@ Rails.application.routes.draw do
 
   root to: 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  namespace :api, defaults: { format: :json } do
+    scope module: :v1 do
+      resources :users, only: [:show]
+    end
+  end
+
 end
