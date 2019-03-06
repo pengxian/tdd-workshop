168fe6bd7218f69cd1cfb829458a767be547d27b
pengxian <qpxblue@aliyun.com>
2019-03-06 19:43:13 +0800

    add order model, and the relationship with user

---
+++ app/models/order.rb
@@ -0,0 +1,7 @@
+class Order < ApplicationRecord
+  belongs_to :user
+
+  validates :total, :user_id, presence: true
+  validates :total, numericality: { greater_than_or_equal_to: 0 }, presence: true
+
+end
